79ef22872bb5bcc3f3aba0d76b5d36994a02004b
Sataphon Obra <puye53991@gmail.com>
2023-05-27 13:16:11 +0700

    idc

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <main>
       <h1 className='text-9xl'>Yo</h1>
       <h1 className='text-2xl underline'>yo</h1>
+      <h1 className='text-2xl underline'>fuck</h1>
     </main>
   );
 }
