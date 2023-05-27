4711d68f01b3237125d0b952726d0f565ead87c3
Sataphon Obra <puye53991@gmail.com>
2023-05-27 12:58:04 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import About from "./components/About";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1 className='text-9xl'>Yo</h1>
+      <h1 className='text-2xl underline'>yo</h1>
+    </main>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+// src/components/About.js
+
+export default function About() {
+    return(
+        <section id="about">
+            <div>
+                <h1>hi</h1>
+            </div>
+        </section>
+    );
+}
+
